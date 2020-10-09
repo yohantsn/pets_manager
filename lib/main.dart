a992c1af1929d26efe72a83975876f95d7cc7ebe
yohant <yohant@positivo.com.br>
2020-10-09 15:26:54 -0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pets_manager/core/colors_scheme.dart';
-import 'package:pets_manager/models/user/user_model.dart';
-import 'package:pets_manager/repositories/user/user_repositories.dart';
 import 'package:pets_manager/views/login/apresentacao_view.dart';
 
-void main() {
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     home: ApresentacaoScreen(),
     debugShowCheckedModeBanner: false,
